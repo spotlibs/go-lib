7e1655dab89524043acedb90d9129443db007170
Danial <mdanialrma@gmail.com>
2024-09-09 09:20:25 +0700

    feat: pkg `ctx` that handle metadata within `http.Context`

---
+++ ctx/metadata.go
@@ -0,0 +1,77 @@
+package ctx
+
+import "github.com/goravel/framework/contracts/http"
+
+// metadataKey key to identify that a value in context is set and get from this
+// package.
+const metadataKey = "spotlibs-metadata-key"
+
+// Metadata holds any request-scoped shared data within brispot microservice.
+type Metadata struct {
+	Authorization   string
+	UserAgent       string
+	CacheControl    string
+	ForwardedFor    string
+	RequestFrom     string
+	DeviceId        string
+	App             string
+	VersionApp      string
+	ReqId           string
+	ReqTags         string
+	ReqUser         string
+	ReqNama         string
+	ReqKodeJabatan  string
+	ReqNamaJabatan  string
+	ReqKodeUker     string
+	ReqNamaUker     string
+	ReqJenisUker    string
+	ReqKodeMainUker string
+	ReqKodeRegion   string
+	PathGateway     string
+	ApiKey          string
+}
+
+// ParseRequest return Metadata from given http context but return empty data
+// instead if no data were found.
+func ParseRequest(c http.Context) Metadata {
+	mt, ok := c.Value(metadataKey).(Metadata)
+	if !ok {
+		mt = Metadata{}
+	}
+	return mt
+}
+
+// SetFromRequestHeader set any available metadata from given http context in
+// the request header.
+func SetFromRequestHeader(c http.Context) {
+	mt := Metadata{
+		Authorization:   c.Request().Header("Authorization"),
+		UserAgent:       c.Request().Header("User-Agent"),
+		CacheControl:    c.Request().Header("Cache-Control"),
+		ApiKey:          c.Request().Header("X-Api-Key"),
+		ForwardedFor:    c.Request().Header("X-Forwarded-For"),
+		RequestFrom:     c.Request().Header("X-Request-From"),
+		DeviceId:        c.Request().Header("X-Device-Id"),
+		App:             c.Request().Header("X-App"),
+		VersionApp:      c.Request().Header("X-Version-App"),
+		ReqId:           c.Request().Header("X-Request-Id"),
+		ReqTags:         c.Request().Header("X-Request-Tags"),
+		ReqUser:         c.Request().Header("X-Request-User"),
+		ReqNama:         c.Request().Header("X-Request-Nama"),
+		ReqKodeJabatan:  c.Request().Header("X-Request-Kode-Jabatan"),
+		ReqNamaJabatan:  c.Request().Header("X-Request-Nama-Jabatan"),
+		ReqKodeUker:     c.Request().Header("X-Request-Kode-Uker"),
+		ReqNamaUker:     c.Request().Header("X-Request-Nama-Uker"),
+		ReqKodeMainUker: c.Request().Header("X-Request-Kode-MainUker"),
+		ReqKodeRegion:   c.Request().Header("X-Request-Kode-Region"),
+		ReqJenisUker:    c.Request().Header("X-Request-Jenis-Uker"),
+		PathGateway:     c.Request().Header("X-Path-Gateway"),
+	}
+	c.WithValue(metadataKey, mt)
+}
+
+// GetReqId shortcut of ParseRequest with ReqId to get request id from given
+// http context.
+func GetReqId(c http.Context) string {
+	return ParseRequest(c).ReqId
+}
