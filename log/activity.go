ac9a3094a59a4057eae16c7285f73a8aa0bf0b30
Danial <mdanialrma@gmail.com>
2024-09-23 10:50:53 +0700

    feat: log writer for Activity, Runtime & Worker

---
+++ log/activity.go
@@ -0,0 +1,40 @@
+package log
+
+import (
+	"context"
+	"sync"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	actOnce   sync.Once
+	actZapLog *zap.Logger // actZapLog use zap log to log message, to benefit from its buffer.
+)
+
+type actLogger struct{}
+
+func (l actLogger) Info(m M) {
+	if !isOff.Load() {
+		actZapLog.Info("", zap.Any("payload", m))
+	}
+}
+
+// Activity start ActLogger.
+func Activity(c ...context.Context) ActLogger {
+	actOnce.Do(func() {
+		// setup log writer
+		actLogWriter := &writer{wr: setupLog("activity")}
+		// setup zap log
+		core := zapcore.NewCore(getZapJsonEncoder(), zapcore.AddSync(actLogWriter), zapcore.InfoLevel)
+		actZapLog = zap.New(core)
+		// add to clean up task
+		cleanupTasks = append(cleanupTasks, func() {
+			_ = actZapLog.Sync()
+			_ = actLogWriter.Close()
+		})
+	})
+
+	return actLogger{}
+}
---
+++ log/worker.go
@@ -0,0 +1,56 @@
+package log
+
+import (
+	"context"
+	"sync"
+
+	"github.com/spotlibs/go-lib/ctx"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var (
+	wrkOnce   sync.Once
+	wrkZapLog *zap.Logger // wrkZapLog use zap log to log message, to benefit from its buffer.
+)
+
+type wrkLogger struct {
+	reqId string
+}
+
+func (l wrkLogger) Info(m M) {
+	if !isOff.Load() {
+		// add request id
+		m["request-id"] = l.reqId
+		wrkZapLog.Info("", zap.Any("payload", m))
+	}
+}
+
+// Worker start WorkLogger.
+func Worker(c ...context.Context) WorkLogger {
+	wrkOnce.Do(func() {
+		// setup log writer
+		wrkLogWriter := &writer{wr: setupLog("worker")}
+		// setup zap log
+		core := zapcore.NewCore(getZapJsonEncoder(), zapcore.AddSync(wrkLogWriter), zapcore.InfoLevel)
+		wrkZapLog = zap.New(core)
+		// add to clean up task
+		cleanupTasks = append(cleanupTasks, func() {
+			_ = wrkZapLog.Sync()
+			_ = wrkLogWriter.Close()
+		})
+	})
+
+	// - Start embedding any necessary metadata from context
+
+	var reqId string
+	if len(c) > 0 {
+		reqId = ctx.GetReqId(c[0])
+
+		// add any other metadata here
+	}
+
+	// - End embedding any necessary metadata from context
+
+	return wrkLogger{reqId: reqId}
+}
