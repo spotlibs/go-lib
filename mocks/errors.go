31a84a322a05236e5ae99685ac280ba988ddc363
Mufthi Ryanda <77824812+mufthiryanda@users.noreply.github.com>
2026-01-19 11:15:31 +0700

    feat : add mocks

---
+++ mocks/errors.go
@@ -0,0 +1,19 @@
+package mocks
+
+import "errors"
+
+const (
+	ErrExpectedNetwork = "called error expected network"
+	ErrExpectedInfra   = "called error expected infra"
+)
+
+func ProduceSampleError(errs ...error) error {
+	var errMsg string
+	if len(errs) == 0 {
+		for _, err := range errs {
+			errMsg += err.Error() + "\n"
+		}
+		return errors.New(errMsg)
+	}
+	return errors.New(ErrExpectedNetwork)
+}
