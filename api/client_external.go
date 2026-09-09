d760327b49573f839bcb8a14e88e1074820576bf
Made Mas Adi Winata <m45adiwinata@gmail.com>
2026-09-09 10:27:17 +0700

    extend timeout

--- api/client_external.go
+++ api/client_external.go
@@ -36,7 +36,7 @@ func NewHTTPClientExternal() HTTPClientExternal {
 
 	var client http.Client
 	client.Transport = &trans
-	client.Timeout = 30 * time.Second
+	client.Timeout = 60 * time.Second
 
 	return &httpClientExternal{cl: &client}
 }
